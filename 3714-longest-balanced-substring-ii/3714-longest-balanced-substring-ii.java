8539d1634e0df8b76f34de101bafe0b6fb00251f
Shivang Mangal <Shivangmangal01@gmail.com>
2026-02-13 22:27:46 +0530

    Time: 411 ms (20.47%), Space: 148.9 MB (70.08%) - LeetHub

---
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.java
@@ -0,0 +1,105 @@
+import java.util.*;
+
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int ans = 1;
+
+        // -------------------------
+        // Case 1: Single character
+        // -------------------------
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+                ans = Math.max(ans, count);
+            } else {
+                count = 1;
+            }
+        }
+
+        // -------------------------
+        // Case 2: All three equal
+        // -------------------------
+        ans = Math.max(ans, threeEqual(s));
+
+        // -------------------------
+        // Case 3: Exactly two equal
+        // -------------------------
+        ans = Math.max(ans, twoEqual(s, 'a', 'b'));
+        ans = Math.max(ans, twoEqual(s, 'a', 'c'));
+        ans = Math.max(ans, twoEqual(s, 'b', 'c'));
+
+        return ans;
+    }
+
+    private int threeEqual(String s) {
+        Map<Long, Integer> map = new HashMap<>();
+        int a = 0, b = 0, c = 0;
+        int max = 0;
+
+        map.put(0L, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == 'a') a++;
+            else if (ch == 'b') b++;
+            else c++;
+
+            int d1 = a - b;
+            int d2 = a - c;
+
+            long key = (((long)d1) << 32) | (d2 & 0xffffffffL);
+
+            if (map.containsKey(key)) {
+                max = Math.max(max, i - map.get(key));
+            } else {
+                map.put(key, i);
+            }
+        }
+
+        return max;
+    }
+
+    private int twoEqual(String s, char x, char y) {
+        int max = 0;
+        int start = 0;
+
+        while (start < s.length()) {
+            // skip invalid chars
+            while (start < s.length() &&
+                  s.charAt(start) != x &&
+                  s.charAt(start) != y) {
+                start++;
+            }
+
+            if (start >= s.length()) break;
+
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, start - 1);
+
+            int diff = 0;
+            int i = start;
+
+            while (i < s.length() &&
+                  (s.charAt(i) == x || s.charAt(i) == y)) {
+
+                if (s.charAt(i) == x) diff++;
+                else diff--;
+
+                if (map.containsKey(diff)) {
+                    max = Math.max(max, i - map.get(diff));
+                } else {
+                    map.put(diff, i);
+                }
+
+                i++;
+            }
+
+            start = i;
+        }
+
+        return max;
+    }
+}
