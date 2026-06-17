afe11a23ad17c0e67c0b6125f9287e636d981c3f
Shivang Mangal <Shivangmangal01@gmail.com>
2026-06-17 20:29:55 +0530

    Time: 494 ms (20.83%), Space: 25.4 MB (27.08%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        n = len(s)
+        lengths = [0] * (n + 1)
+        for i, ch in enumerate(s):
+            cur = lengths[i]
+            if 'a' <= ch <= 'z':
+                lengths[i + 1] = cur + 1
+            elif ch == '*':
+                lengths[i + 1] = max(0, cur - 1)
+            elif ch == '#':
+                lengths[i + 1] = cur * 2
+            else:
+                lengths[i + 1] = cur
+        if k >= lengths[n]:
+            return '.'
+        for i in range(n - 1, -1, -1):
+            ch = s[i]
+            prev_len = lengths[i]
+
+            if 'a' <= ch <= 'z':
+                if k == prev_len:
+                    return ch
+            elif ch == '#':
+                if k >= prev_len:
+                    k -= prev_len
+            elif ch == '%':
+                k = prev_len - 1 - k
+        return '.'
