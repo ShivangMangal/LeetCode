2791adb22eb3574e55cbd03b53ab734fc8325d0b
Shivang Mangal <Shivangmangal01@gmail.com>
2026-08-01 14:12:33 +0530

    Time: 4 ms (39.38%), Space: 19.9 MB (27.38%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        memo = {}
+
+        def dp(i, j):
+            if i == j:
+                return nums[i]
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            memo[(i, j)] = max(
+                nums[i] - dp(i + 1, j),
+                nums[j] - dp(i, j - 1)
+            )
+            return memo[(i, j)]
+
+        return dp(0, len(nums) - 1) >= 0
